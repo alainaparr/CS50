0178bb8e35f6750f9f8e868b9b1b20af08245d39
alainaparr <113211566+alainaparr@users.noreply.github.com>
2022-12-17 23:46:55 -0600

    Create 8.sql

---
+++ week 7/movies/8.sql
@@ -0,0 +1,5 @@
+-- list names of all people in toy story
+SELECT name FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON movies.id = stars.movie_id
+WHERE movies.title = "Toy Story";
